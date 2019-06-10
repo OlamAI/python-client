f5d37089be78279b750f84a44350de10ea038caf
Zac Holland <zacharyholland@gmail.com>
2019-06-09 18:32:37 -0600

    Working lockstepping!

--- Collective.py
+++ Collective.py
@@ -8,6 +8,7 @@ import collective_pb2_grpc
 
 
 PROD_SERVER_ADDR = "" # TODO
+lock = threading.Lock()
 
 def defaultModelFunc(obsv):
   raise Exception('You need to pass in a model function.')
@@ -15,52 +16,26 @@ def defaultModelFunc(obsv):
 # Create an iterator to let us send a stream
 class MySmarterRequestIterator(object):
   def __init__(self):
-      self._lock = threading.Lock()
       self._responses = []
 
   def __iter__(self):
       return self
 
   def _next(self):
-      # some logic that depends upon what responses have been seen
-      # before returning the next request message
-      # with selsdelFunc resp: ', resp)
-      print("_next")
-
-      
-      while True:
-        with self._lock:
-          if len(self._responses) > 0:
-            print("Sending something...")
-            response = self._responses.pop(0)
-            # actions = [collective_pb2.Action(id="0", action=0, direction=0)]
-            # actionPacket = collective_pb2.ActionPacket(actions=actions)
-            return response
-          else:
-            # print("Nothing here, waiting...")
-            time.sleep(0.01)
-
-      # actions = [collective_pb2.Action(id="0", action=0, direction=0)]
-      # actionPacket = collective_pb2.ActionPacket(actions=actions)
-      # return actionPacket
-      # print(self._modelFunc)
-      # try:
-      #   resp = self._modelFunc('test')
-      #   print(resp)
-      #   return resp
-      # except:
-      #   print("There was an error calling the function")
-      #   raise Exception('There was an error calling the function')
+    # Acquire the lock
+    lock.acquire()
+    response = self._responses.pop(0)
+    return response
 
   def __next__(self):  # Python 3
       return self._next()
 
   def add_response(self, response):
       print("Adding a response...")
-      with self._lock:
-        print("Got lock...")
-        self._responses.append(response)
-        print("Response added!")
+      # with self._lock:
+      print("Got lock...")
+      self._responses.append(response)
+      print("Response added!")
   
 
 def connectRemoteModel(secret, modelName, modelFunc, addr=PROD_SERVER_ADDR):
@@ -73,6 +48,7 @@ def connectRemoteModel(secret, modelName, modelFunc, addr=PROD_SERVER_ADDR):
 
   responses = stub.ConnectRemoteModel(requestItt, metadata=metadata)
   t1 = time.time()*1000.0
+  lock.acquire()
   try:
       for response in responses:
         t2 = time.time()*1000.0
@@ -83,6 +59,7 @@ def connectRemoteModel(secret, modelName, modelFunc, addr=PROD_SERVER_ADDR):
         actions = [collective_pb2.Action(id="0", action=0, direction=0)]
         actionPacket = collective_pb2.ActionPacket(actions=actions)
         requestItt.add_response(actionPacket)
+        lock.release()
   #     for obsvPacket in stub.CreateRemoteModel(metadata=metadata):
   #         action = 0
   #         direction = 0
