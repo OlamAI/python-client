7125024d6198c47a304b3b63ee25dc8a9e989fbd
Zac Holland <zacharyholland@gmail.com>
2019-07-29 16:39:14 -0700

    implemented queue instead of other weird method

--- collective.py
+++ collective.py
@@ -2,6 +2,7 @@ import threading
 import grpc
 import sys
 import time
+from queue import Queue
 sys.path.insert(0, './api')
 import collective_pb2
 import collective_pb2_grpc
@@ -11,43 +12,17 @@ PROD_SERVER_ADDR = "" # TODO
 
 def defaultModelFunc(obsv):
   raise Exception('You need to pass in a model function.')
-
-# Create an iterator to let us send a stream
-class MySmarterRequestIterator(object):
-  def __init__(self):
-      self._request = None
-
-  def __iter__(self):
-      return self
-
-  def _next(self):
-    # Block until we get the lock, then return the request
-    self._lock.acquire()
-    return self._request
-
-  def __next__(self):  # Python 3
-    return self._next()
-
-  def set_request(self, request):
-    self._request = request
-
-  def set_lock(self, lock):
-    self._lock = lock
   
-
 def connectRemoteModel(secret, modelFunc, addr=PROD_SERVER_ADDR):
   # Create the stub
   channel = grpc.insecure_channel(addr)
   stub = collective_pb2_grpc.CollectiveStub(channel)
   metadata = [('model-secret', secret)]
-  # Create and acquire the lock
-  lock = threading.Lock()
-  lock.acquire()
   # Create the iterator
-  requestItt = MySmarterRequestIterator()
-  requestItt.set_lock(lock)
+  request_queue = Queue()
+  request_iter = iter(request_queue.get, object())
   # Connect
-  responses = stub.ConnectRemoteModel(requestItt, metadata=metadata)
+  responses = stub.ConnectRemoteModel(request_iter, metadata=metadata)
   # Try/catch cleanup for Notebook
   try:
       for response in responses:
@@ -57,12 +32,8 @@ def connectRemoteModel(secret, modelFunc, addr=PROD_SERVER_ADDR):
           actions.append(modelFunc(obsv))
         actionPacket = collective_pb2.ActionPacket(actions=actions)
         # Set the request to the iterator
-        requestItt.set_request(actionPacket)
-        # Release lock so the iterator can make a passthrough
-        lock.release()
+        request_queue.put(actionPacket)
   except KeyboardInterrupt:
-    # Release lock
-    lock.release()
     # Send cancel to the server
     responses.cancel()
 
